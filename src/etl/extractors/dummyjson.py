0b867e78016e72ec45d1b6483dceec1f8375f9ab
Sumit Achar <sumitachar89@gmail.com>
2026-09-17 00:24:49 +0530

    data pagination

--- src/etl/extractors/dummyjson.py
+++ src/etl/extractors/dummyjson.py
@@ -5,20 +5,44 @@ import requests
 
 
 API_URL = "https://dummyjson.com/products"
+LIMIT = 10
 
 
 def fetch_products():
     print("🚀 Fetching products from DummyJSON...")
 
-    response = requests.get(API_URL)
+    all_products = []
+    skip = 0
 
-    print("Status Code:", response.status_code)
+    while True:
+        params = {
+            "limit": LIMIT,
+            "skip": skip,
+        }
 
-    response.raise_for_status()
+        response = requests.get(API_URL, params=params)
 
-    data = response.json()
+        print(
+            f"Fetching: limit={LIMIT}, "
+            f"skip={skip}, "
+            f"Status={response.status_code}"
+        )
 
-    print("Total Products:", data["total"])
+        response.raise_for_status()
+
+        data = response.json()
+        products = data["products"]
+
+        all_products.extend(products)
+
+        print(f"Received {len(products)} products")
+
+        if skip + LIMIT >= data["total"]:
+            break
+
+        skip += LIMIT
+
+    print(f"✅ Total products fetched: {len(all_products)}")
 
     raw_dir = Path("data/raw")
     raw_dir.mkdir(parents=True, exist_ok=True)
@@ -26,11 +50,11 @@ def fetch_products():
     file_path = raw_dir / "dummyjson_products.json"
 
     with open(file_path, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=2)
+        json.dump(all_products, file, indent=2)
 
     print(f"✅ Raw data saved successfully: {file_path}")
 
-    return data
+    return all_products
 
 
 if __name__ == "__main__":
